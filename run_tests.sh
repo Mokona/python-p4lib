965478ecf8b71987a33dde5d7ee8bd8e4681bce4
Sylvain Glaize <mokona@puupuu.org>
2014-10-30 21:47:08 +0100

    Removed the hardcoded path for running tests with bash

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,15 @@
-#!bash
-export PYTHONPATH=/opt/p4lib/lib/:$PYTHONPATH
-export PYTHONPATH=/opt/p4lib/px/:$PYTHONPATH
-export PYTHONPATH=/opt/p4lib/test/functionnal/:$PYTHONPATH
+#!/bin/bash
+DIR=$(dirname "${BASH_SOURCE[0]}")
+echo $DIR
+pushd $DIR
+CURRENT_DIR=`pwd` 
+
+export PYTHONPATH=$CURRENT_DIR/lib/:$PYTHONPATH
+export PYTHONPATH=$CURRENT_DIR/px/:$PYTHONPATH
+export PYTHONPATH=$CURRENT_DIR/test/functionnal/:$PYTHONPATH
 echo $PYTHONPATH
-python /opt/p4lib/test/functionnal/test.py -x test_px_diff -x test_px_backout -x test_px_genpatch -x test_px_options
+
+python $CURRENT_DIR/test/functionnal/test.py -x test_px_diff -x test_px_backout -x test_px_genpatch -x test_px_options
+
+popd
+
