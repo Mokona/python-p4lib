7e8b4790962c9aef5c3b2fbbd7a7076b050a7bb7
Sylvain Glaize <mokona@puupuu.org>
2014-11-09 00:10:02 +0100

    Tests for Change on every situation.

--- test/mocked/p4lib_change_test.py
+++ test/mocked/p4lib_change_test.py
@@ -5,10 +5,32 @@ from test_utils import change_stdout, change_stdout_list
 from p4lib_opened_test import PX_AND_PX2_DEFAULT_CHANGE
 
 
+# Note: a better way should be to mock where and opened functions called
+# by change() when it needs information
+# At now, we're feeding _run() side_effects.
+# That's not really unit testing, as it's based on opened() and where()
+# functioning.
+
+
 TEMPORARY_FILENAME = "tempFile"
 CHANGELIST_DESCRIPTION = "A description"
+CHANGELIST_NEW_DESCRIPTION = "A new description"
 
 CHANGE_CREATED = r"Change 1234 created."
+CHANGE_UPDATED = r"Change 1234 updated."
+CHANGE_UPDATED_WITH_COMMENTS = r"Change 1234 updated, and blah."
+CHANGE_DELETED = r"Change 1234 deleted."
+CHANGE_INFORMATION = """Change:\t1234
+Date:\t2002/05/08 23:24:54
+User:\ttrent
+Status:\tpending
+Description:
+\tA description
+
+Files:
+\t//depot/file.txt\t# edit
+"""
+
 WHERE_RESULT = r"//depot/foo/file.py //trent/foo/file.py c:\trent\foo\file.py"
 
 
@@ -19,12 +41,15 @@ class ChangeTestCase(unittest.TestCase):
                                          return_value=TEMPORARY_FILENAME)
         p4lib._removeTemporaryForm = Mock(spec='p4lib._removeTemporaryForm')
 
-    def __assert_change_for_creation(self, result):
+    def __assert_called_with_tempfile(self):
         p4lib._run.assert_called_with(['p4',
                                        'change',
                                        '-i',
                                        '<',
                                        TEMPORARY_FILENAME])
+
+    def __assert_change_for_creation(self, result):
+        self.__assert_called_with_tempfile()
 
         self.assertIn("change", result)
         self.assertEqual(1234, result["change"])
@@ -80,3 +105,100 @@ class ChangeTestCase(unittest.TestCase):
         self.assertIn(CHANGELIST_DESCRIPTION, form)
         self.assertIn("//depot/foo/file.py", form)
 
+    def test_can_get_information(self):
+        change_stdout(CHANGE_INFORMATION)
+
+        p4 = p4lib.P4()
+
+        result = p4.change(change=1234)
+        p4lib._run.assert_called_with(['p4', 'change', '-o', '1234'])
+
+        self.assertEqual(1234, result["change"])
+        self.assertEqual("pending", result["status"])
+        self.assertEqual(CHANGELIST_DESCRIPTION, result["description"])
+        self.assertEqual("trent", result["user"])
+        self.assertEqual("2002/05/08 23:24:54", result["date"])
+
+    def test_can_change_description(self):
+        change_stdout_list([CHANGE_INFORMATION, CHANGE_UPDATED])
+
+        p4 = p4lib.P4()
+
+        result = p4.change(change=1234,
+                           description=CHANGELIST_NEW_DESCRIPTION)
+
+        self.__assert_called_with_tempfile()
+
+        self.assertEqual("updated", result["action"])
+        self.assertEqual(1234, result["change"])
+
+        form = self.__get_form()
+        self.assertIn(CHANGELIST_NEW_DESCRIPTION, form)
+
+    def test_can_change_files(self):
+        change_stdout_list([CHANGE_INFORMATION,
+                            WHERE_RESULT,
+                            CHANGE_UPDATED_WITH_COMMENTS])
+
+        p4 = p4lib.P4()
+
+        result = p4.change(change=1234,
+                           files=["//depot/foo/file.py"])
+
+        self.__assert_called_with_tempfile()
+
+        self.assertEqual("updated", result["action"])
+        self.assertEqual(1234, result["change"])
+        self.assertEqual("and blah", result["comment"])
+
+        form = self.__get_form()
+        self.assertIn(CHANGELIST_DESCRIPTION, form)
+        self.assertIn("//depot/foo/file.py", form)
+
+    def test_can_be_deleted(self):
+        change_stdout(CHANGE_DELETED)
+
+        p4 = p4lib.P4()
+
+        result = p4.change(change=1234, delete=True)
+
+        p4lib._run.assert_called_with(['p4', 'change', '-d', '1234'])
+
+        self.assertEqual("deleted", result["action"])
+        self.assertEqual(1234, result["change"])
+
+    def test_cannot_specify_delete_with_files_or_description(self):
+        p4 = p4lib.P4()
+
+        self.assertRaises(p4lib.P4LibError, p4.change,
+                          change=1234,
+                          delete=True,
+                          files=[])
+
+        self.assertRaises(p4lib.P4LibError, p4.change,
+                          change=1234,
+                          delete=True,
+                          descritpion="")
+
+    def test_raw_result(self):
+        change_stdout(CHANGE_DELETED)
+
+        p4 = p4lib.P4()
+        raw_result = p4.change(change=1234, delete=True, _raw=True)
+
+        self.assertIn('stdout', raw_result)
+        self.assertIn('stderr', raw_result)
+        self.assertIn('retval', raw_result)
+
+        self.assertEqual(CHANGE_DELETED, raw_result['stdout'])
+
+    def test_with_options(self):
+        change_stdout(CHANGE_DELETED)
+
+        p4 = p4lib.P4()
+        p4.change(change=1234, delete=True, user='other')
+
+        p4lib._run.assert_called_with(['p4',
+                                       '-u',
+                                       'other',
+                                       'change', '-d', '1234'])
