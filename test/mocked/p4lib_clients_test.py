aa7e86c856b06f06b1a00063e67a59b965e26470
Sylvain Glaize <mokona@puupuu.org>
2014-11-29 23:29:52 +0100

    Added mocked test for Clients command

---
+++ test/mocked/p4lib_clients_test.py
@@ -0,0 +1,48 @@
+import unittest
+import p4lib
+from mock import Mock
+from test_utils import change_stdout, test_options
+
+
+CLIENTS_OUTPUT = """Client trentm-ra 2002/03/18 root c:\\trentm\\ 'By trentm.'
+Client mokona-local 2014/11/28 root /home/mokona/workspace 'By mokona.'
+"""
+
+
+class ClientsTestCase(unittest.TestCase):
+    def setUp(self):
+        p4lib._run = Mock(spec='p4lib._run', return_value=("", "", 0))
+        change_stdout(CLIENTS_OUTPUT)
+        self.p4 = p4lib.P4()
+
+    def test_fills_results(self):
+        result = self.p4.clients()
+
+        p4lib._run.assert_called_with(['p4', 'clients'])
+
+        self.assertEqual(2, len(result))
+
+        expected_1 = {'client': 'trentm-ra',
+                      'root': 'c:\\trentm\\',
+                      'update': '2002/03/18',
+                      'description': 'By trentm.'}
+        self.assertEqual(expected_1, result[0])
+
+        expected_2 = {'client': 'mokona-local',
+                      'root': '/home/mokona/workspace',
+                      'update': '2014/11/28',
+                      'description': 'By mokona.'}
+        self.assertEqual(expected_2, result[1])
+
+    def test_raw_result(self):
+        raw_result = self.p4.clients(_raw=True)
+
+        self.assertIn('stdout', raw_result)
+        self.assertIn('stderr', raw_result)
+        self.assertIn('retval', raw_result)
+
+        self.assertEqual(CLIENTS_OUTPUT, raw_result['stdout'])
+
+    def test_with_options(self):
+        test_options(self, "clients",
+                     expected=["clients"])
